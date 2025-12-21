59af0df96f5884c4bc907dd651719564453a70f0
Anish0605 <53084483+Anish0605@users.noreply.github.com>
2025-12-21 10:40:41 +0530

    Create voice.py

---
+++ backend/voice.py
@@ -0,0 +1,10 @@
+#voice.py
+import uuid, os, time, edge_tts
+from fastapi.responses import FileResponse
+
+async def generate_voice_stream(text):
+    temp_filename = f"voice_{uuid.uuid4()}.mp3"
+    # Uses Indian English voice for the OS identity
+    communicate = edge_tts.Communicate(text, "en-IN-PrabhatNeural")
+    await communicate.save(temp_filename)
+    return FileResponse(temp_filename, media_type="audio/mpeg")
